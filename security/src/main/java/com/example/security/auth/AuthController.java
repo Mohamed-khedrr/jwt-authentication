89e67fa0ef58a99e424641426a96ca0a1ed073ef
Mohamed Khedr <khedr66664@gmail.com>
2024-04-06 22:52:07 +0200

    finish jwt config and add end-points for test

---
+++ security/src/main/java/com/example/security/auth/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private  final  AuthService authService ;
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register (@RequestBody RegisterRequest request){
+    return ResponseEntity.ok(authService.register(request)) ;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login (@RequestBody loginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
